1881b3268a4ee1df76d30824006297c4757e979f
wudu <2801259219@qq.com>
2022-04-22 20:53:43 +0800

    fix#fpdf2 api changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='extract-video-ppt',
-    version='1.1.1',
+    version='1.1.2',
     author="wudu",
     author_email="296525335@qq.com",
     description="support export ppt from a video",
--- video2ppt/images2pdf.py
+++ video2ppt/images2pdf.py
@@ -9,8 +9,8 @@ def images2pdf(outpath, images, w = 1920, h = 1080):
     for image in images:
         pdf.add_page(orientation = 'L', format=size, same=False)
         pdf.set_font('helvetica', size = titleH)
-        pdf.cell(400, titleH, os.path.basename(image), 1, 1, 'C')
-        pdf.image(image, 0, titleH + 10, w, h, 'JPG')
+        pdf.cell(w = 400, h = titleH, txt = os.path.basename(image), border = 1, align = 'C')
+        pdf.image(image, x = 0, y = titleH + 10, w = w, h = h, type = 'JPG')
 
     pdf.output(outpath, "F")
     
